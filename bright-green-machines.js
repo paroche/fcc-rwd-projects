8dc0ad82f2089e7f5d1112a00272b07069e90337
Peter Roche <sunmtnsft@aol.com>
2022-01-17 21:57:49 -0700

    Green Machines -- read/set header height in js for css, Centering: #navbar fix for responsive

--- bright-green-machines.js
+++ bright-green-machines.js
@@ -1,6 +1,20 @@
+const body = document.body;
+const header = document.getElementById('header');
 const elem = document.documentElement;
+
+// Adjust for actual header height
+updateHeaderHeight(); // initial
+window.addEventListener('resize', updateHeaderHeight);
 
 // Switch to full screen in iframe
 elem.addEventListener('dblclick', () => {
   if (window.frameElement != null) elem.requestFullscreen();
 }); 
+
+function updateHeaderHeight() {
+  console.log("in updateHeaderHeight");
+  let height = header.offsetHeight;
+  console.log("height: ",height);
+  body.style.setProperty("--header-height", height+"px");
+  body.style.setProperty("--neg-header-height", -height+"px");
+}
--- centering-text-and-images.js
+++ centering-text-and-images.js
@@ -2,7 +2,13 @@ const elem = document.documentElement;
 const body = document.body;
 
 // Switch to full screen in iframe
-// for some reason the nav block is blacked out in both Chrome and Firefox
+// for some reason the nav block was blacked out in both Chrome and Firefox until I added an explicit background: white in css (only need in iframe)
 elem.addEventListener('dblclick', () => {
   if (window.frameElement != null) elem.requestFullscreen();
 }); 
+
+// body.addEventListener('click', showWidth);
+
+function showWidth() {
+  console.log(window.innerWidth);
+}
